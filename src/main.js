529ee27e8194ed706326d6527a7aae86281f7b0f
U-FISHBOWLDOMAIN\lbeatty <lbeatty@lbeattynote.FISHBOWLDOMAIN.com>
2016-06-04 21:01:09 -0500

    author table in place

--- src/main.js
+++ src/main.js
@@ -2,35 +2,43 @@
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
+var Authors = require('./components/authors/authorPage');
 var About = require('./components/about/aboutPage');
 var Header = require('./components/common/header');
 
-(function(win){
-  "use strict";
-  var App = React.createClass({
-    render: function(){
-      var Child;
 
-      switch(this.props.route){
-        case 'about': Child = About; break;
-        default: Child = Home;
-      }
-      return (
-        <div>
-          <Child/>
-          <Header/>
-        </div>
-      );
-    }
-  });
+(function(win) {
+          "use strict";
+          var App = React.createClass({
+               render: function() {
+                    var Child;
 
-  function render(){
-    var route = win.location.hash.substr(1);
-    React.render(<App route={route} />, document.getElementById('app'));
-  }
+                    switch (this.props.route) {
+                         case 'about':
+                              Child = About;
+                              break;
+                         case 'authors':
+                              Child = Authors;
+                              break;
+                         default:
+                              Child = Home;
+                    }
+                    return (
+                         <div>
+                            <Header/>
+                            <Child/>
+                         </div>
+                    );
+               }
+          });
 
-  win.addEventListener('hashchage', render);
-  render();
-})(window);
+          function render() {
+               var route = win.location.hash.substr(1);
+               React.render( <App route = {route}/>, document.getElementById('app'));
+         }
 
-// React.render(<Home />, document.getElementById('app'));
+               win.addEventListener('hashchage', render);
+               render();
+          })(window);
+
+     // React.render(<Home />, document.getElementById('app'));
